4d504cb23efdc8e731f5aade91b34fd4bae2eadd
zeid <zeidhassan4@gmail.com>
2023-12-12 23:49:45 +0300

    Complited assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
+
+let random = Math.ceil(Math.random())
+Number.isInteger(random)
+
+
+let num3 = 29
+let num4 = 5
+let mod = num3 % num4
+
+let max = Math.max( 5, 1, 10, 15, 20, 13)
